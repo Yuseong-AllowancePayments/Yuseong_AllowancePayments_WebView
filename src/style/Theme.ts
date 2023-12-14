59d1e9983c0bd9c77ec19f986ca65b2f9b5ec367
inung1004 <dksdbswl10430@naver.com>
2023-12-14 17:06:36 +0900

    setting :: global style 세팅

---
+++ src/style/Theme.ts
@@ -0,0 +1,22 @@
+const colors = {
+  primary: "#2F68C2",
+  primary_hover: "#3C74C9",
+  primary_background: "#E6EDF8",
+  gray900: "#262726",
+  gray800: "#3F403F",
+  gray700: "#585A58",
+  gray600: "#727472",
+  gray500: "#8B8D8B",
+  gray400: "#A5A7A5",
+  gray300: "#BFC0BF",
+  gray200: "#E5E6E5",
+  gray100: "#F5F5F5",
+  WHITE: "#FFFFFF",
+  BLACK: "#000000",
+  red: "#D6403C",
+  green: "#2FC23E",
+};
+
+export const theme = {
+  colors: colors,
+};
