4d2e83eeb89f8a261ab6b7c946ec2b14ef8f5028
inung1004 <dksdbswl10430@naver.com>
2023-12-29 10:57:25 +0900

    feat :: 엑셀 파싱 훅 생성

---
+++ src/hooks/index.ts
@@ -0,0 +1,37 @@
+import axios, { AxiosError } from "axios";
+import { useMutation } from "react-query";
+import { getExcelResponseDto } from "../model/response";
+import toast from "react-hot-toast";
+
+const BASE_URL = process.env.REACT_APP_PUBLIC_BASE_URL;
+
+export const useParsingExcel = () => {
+  return useMutation(
+    (formData: FormData) =>
+      axios.post<getExcelResponseDto>(`${BASE_URL}/excel/parse`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }),
+    {
+      onSuccess: (response) => {
+        toast.success("성공", { duration: 1000 });
+        if (window.webkit) {
+          window.webkit.messageHandlers.btnClick.postMessage(response.data);
+        }
+      },
+      onError: (err: AxiosError) => {
+        if (err.response) {
+          switch (err.response.status) {
+            case 400:
+              toast.error("정보를 다시 확인해 주세요.");
+              break;
+            case 401:
+              toast.error("개발자에게 문의해 주세요.");
+              break;
+          }
+        } else toast.error("네트워크 연결 상태를 확인해 주세요.");
+      },
+    }
+  );
+};
